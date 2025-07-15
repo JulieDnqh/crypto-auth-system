38961ffc2f9f49b30898d913b561c38575f7451e
22127475 <huycan1211@gmail.com>
2025-07-15 11:30:17 +0700

    update log (connect database and write log local)

--- modules/utils/logger.js
+++ modules/utils/logger.js
@@ -1,18 +1,34 @@
 
+const prisma = require('../config/db');
 const fs = require('fs');
 const path = require('path');
 
 const logFilePath = path.join(__dirname, '..', '..', 'security.log');
 
-const log = (email, action, status, details = '') => {
+const log = async (email, action, status, details = '') => {
     const timestamp = new Date().toISOString();
     const logEntry = `[${timestamp}] [${email}] [${action}] [${status}] ${details}\n`;
 
+    // Ghi vào file security.log
     fs.appendFile(logFilePath, logEntry, (err) => {
         if (err) {
             console.error('Failed to write to log file:', err);
         }
     });
+
+    // Ghi vào database
+    try {
+        await prisma.logActivity.create({
+            data: {
+                email,
+                action,
+                status,
+                details,
+            },
+        });
+    } catch (error) {
+        console.error('Failed to write log to database:', error);
+    }
 };
 
 module.exports = log;
